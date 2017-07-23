889c6950b1f1a9d04263d92668b38f0d0b69500d
Harrison Shoebridge <harrison@theshoebridges.com>
2017-07-23 16:18:04 -0700

    Create file retrieval middleware

--- server.go
+++ server.go
@@ -10,6 +10,8 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+type HandlerWithFile func(http.ResponseWriter, *http.Request, []byte)
+
 type Server struct {
 	Public string
 }
@@ -18,39 +20,42 @@ func (s Server) Router() http.Handler {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/edit/{rest:.*}", s.handleEdit).Methods("GET", "PUT")
-	r.HandleFunc("/{rest:.*}", s.handleShow).Methods("GET")
+
+	r.HandleFunc(
+		"/{rest:.*}",
+		s.middlewareGetFile(s.handleShow),
+	).Methods("GET")
 
 	return r
+}
+
+func (s Server) middlewareGetFile(f HandlerWithFile) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fpath := mux.Vars(req)["rest"]
+		w.Header().Set("Content-Type", "text/html")
+
+		raw, err := s.file(fpath)
+		if err != nil {
+			fmt.Fprintln(w, "Could not find file")
+			return
+		}
+
+		f(w, req, raw)
+	}
 }
 
 func (s Server) handleEdit(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "Editing: "+mux.Vars(req)["rest"])
 }
 
-func (s Server) handleShow(w http.ResponseWriter, req *http.Request) {
-	fpath := mux.Vars(req)["rest"]
+func (s Server) handleShow(w http.ResponseWriter, req *http.Request, raw []byte) {
+	md := blackfriday.MarkdownCommon(raw)
 
-	f, err := s.file(fpath)
-
-	if err != nil {
-		fmt.Fprintln(w, "Could not find file")
-		return
-	}
-
-	w.Header().Set("Content-Type", "text/html")
-
-	fmt.Fprintf(w, "%s", f)
+	fmt.Fprintf(w, "%s", md)
 }
 
 func (s Server) file(fpath string) ([]byte, error) {
 	p := path.Join(s.Public, path.Clean(fpath))
 
-	raw, err := ioutil.ReadFile(p)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	md := blackfriday.MarkdownCommon(raw)
-
-	return md, nil
+	return ioutil.ReadFile(p)
 }
