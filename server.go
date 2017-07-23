4162872cb6ddc08a8d2f70fb0daefdf03e631492
Harrison Shoebridge <harrison@theshoebridges.com>
2017-07-23 16:04:50 -0700

    Serve files

--- server.go
+++ server.go
@@ -2,9 +2,12 @@ package serkis
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"path"
 
 	"github.com/gorilla/mux"
+	"github.com/russross/blackfriday"
 )
 
 type Server struct {
@@ -14,11 +17,40 @@ type Server struct {
 func (s Server) Router() http.Handler {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", s.homeHandler)
+	r.HandleFunc("/edit/{rest:.*}", s.handleEdit).Methods("GET", "PUT")
+	r.HandleFunc("/{rest:.*}", s.handleShow).Methods("GET")
 
 	return r
 }
 
-func (s Server) homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!\nServing: %s\n", s.Public)
+func (s Server) handleEdit(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "Editing: "+mux.Vars(req)["rest"])
+}
+
+func (s Server) handleShow(w http.ResponseWriter, req *http.Request) {
+	fpath := mux.Vars(req)["rest"]
+
+	f, err := s.file(fpath)
+
+	if err != nil {
+		fmt.Fprintln(w, "Could not find file")
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+
+	fmt.Fprintf(w, "%s", f)
+}
+
+func (s Server) file(fpath string) ([]byte, error) {
+	p := path.Join(s.Public, path.Clean(fpath))
+
+	raw, err := ioutil.ReadFile(p)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	md := blackfriday.MarkdownCommon(raw)
+
+	return md, nil
 }
